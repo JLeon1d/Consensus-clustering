4147fdee4ff75640fc0742087088e3657d94dc22
Andrey Golov <andrei.golov.13@mail.ru>
2026-02-27 11:02:01 +0300

    fix

--- original_code/util/optSigma.m
+++
@@ -1,9 +0,0 @@
-function sigma = optSigma(X)
-    N = size(X,1);
-    dist = EuDist2(X,X); 
-    dist = reshape(dist,1,N*N);
-    sigma = median(dist);
-     if sigma==0
-         dist2=dist(find(dist>0));
-         sigma=median(dist2);
-     end
